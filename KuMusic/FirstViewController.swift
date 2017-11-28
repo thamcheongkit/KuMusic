9b9ce3f96651b944537c1b9df2603fa8902e698e
ck <thamcheongkit@gmail.com>
2017-11-28 16:29:26 +0800

    Initial Commit

---
+++ KuMusic/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  KuMusic
+//
+//  Created by ck on 28/11/2017.
+//  Copyright © 2017 ck. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
